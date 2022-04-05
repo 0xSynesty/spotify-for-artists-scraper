3dd4737161ad6071f998e66178927c81de697cf9
0xSynesty <antoine.labonne1@gmail.com>
2022-04-05 19:34:28 +0200

    Readability improvements

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Spotify For Artists Scraper
 
+## ! This code is obsolete and needs to be re-adapted !
 
-This bot uses a single Spotify For Artists account and scrapes other artists daily statistics by running continuously.
+This bot uses a single Spotify For Artists account withs login, password and artist ID located in <code>logs.txt</code> and scrapes other artists daily statistics by running continuously.
 
 Spotify statistics are updated once a day at a relatively random time so the bot constantly checks if they were updated.
 
-Once it finishes getting daily streaming statistics for all Artists specified in <code>artistes.txt</code>, it sends them to the discord server which hook is specified in <code>discord_hook.txt</code>.
+Once it finishes getting daily streaming statistics for all Artists specified in <code>artists.txt</code>, it sends them to the discord server which hook is specified in <code>discord_hook.txt</code>.
+
 
 ***You will need the chromedriver.exe corresponding to your version of Google Chrome in order to make the bot work, the driver is available here : https://chromedriver.chromium.org/downloads***
--- main.py
+++ main.py
@@ -1,44 +1,54 @@
 import time
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+
 from dhooks import Webhook, Embed, File
+
 import os
 
 with open('discord_hook.txt', 'r') as file_hook:
 	hook = file_hook.read().strip()
 
-def corr_hook(hook):
+def fix_hook(hook):
 	if "app" in hook:
 		hook = hook[:15]+hook[18:]
 		return hook
 	else: return hook
 
-hook_corr = corr_hook(hook)
-hook_fin = Webhook(hook_corr)
+hook_corrected = fix_hook(hook)
+hook_fin = Webhook(hook_corrected)
+
+# Starting Discord embed
 embed = Embed()
 stats_ups = 'https://cdn.discordapp.com/attachments/739982164933738538/785176029530030120/statups.gif'
 
 
 total_streams = 0
+
 artists_list = []
 
-artists = open('artistes.txt', 'r').read().splitlines()
-artists = [artiste for artiste in artists if artiste != ""]
+artists = open('artists.txt', 'r').read().splitlines()
+artists = [artist for artist in artists if artist != ""]
 
 logs = open('logs.txt', 'r').read().split(':')
 
-#options = webdriver.ChromeOptions()
-#options.add_argument("headless")
-#options.add_argument('log-level=3')
-#options=options
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+options.add_argument('log-level=3')
+options=options
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 
 
 def connexion():
+		"""
+		Connects to the main account
+		"""
+
 		wait = WebDriverWait(driver, 30)
 		driver.get('https://accounts.spotify.com/fr/login?continue=https:%2F%2Fartists.spotify.com%2F')
 		driver.refresh()
@@ -51,12 +61,17 @@ def connexion():
 		url = driver.current_url
 		log_ID = url[37:len(url)-5]
 		logs.append(log_ID)
-		print(logs)
+
 		time.sleep(2)
 		driver.get('https://artists.spotify.com/c/artist/'+logs[2]+'/audience?audience-filter=streams')
-		print('Connecté')
+
+		print('Connected')
 
 def streams_artiste_logged():
+		"""
+		Gets statistics from the on which we are logged
+		"""
+
 		global total_streams
 		wait = WebDriverWait(driver, 30)
 		wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'CircleShadow_shadow__G-Qkd')))
@@ -73,7 +88,11 @@ def streams_artiste_logged():
 			stats.write(artiste_logged+' : '+streams_logged+' stream.\n')
 		embed.add_field(name=artiste_logged, value=streams_logged)
 
-def streams_artists_liste():
+def streams_artists_list():
+		"""
+		Gets statistics for the artists in the artists.txt file
+		"""
+
 		global artists
 		global total_streams
 		for i in range(0, len(artists)):
@@ -86,18 +105,26 @@ def streams_artists_liste():
 			aStreams = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'TimelineTooltip_timeline_tooltip-value__2ipLx')))
 			aStreams = aStreams.text
 			print(artists[i], ' : ',aStreams)
+
+
 			if "k" in aStreams.lower():
 						total_streams += float(aStreams.replace(',', '.').replace('k',''))
 			else :
 						total_streams += float(aStreams)/1000
 			aClose = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="timeline"]/section/div/div/div[2]/div[2]/button')))
 			aClose.click()
+
+			# Adding artist statistics
 			if i < 23:
 				embed.add_field(name=artists[i], value=aStreams)
 			with open('daily_stats.txt', 'a') as stats:
 				stats.write(artists[i]+' : '+aStreams+' streams.\n')
 
 def reset():
+	"""
+	Resets global variable to avoid conflicts if something fails while checking for new statistics
+	"""
+
 	global total_streams
 	global embed
 	total_streams = 0
@@ -105,17 +132,23 @@ def reset():
 
 def send():
 	with open('daily_stats.txt', 'a') as stats:
-		stats.write('\n\nTotal des streams du jour : {}k\nSoit {}€ avec un ratio de 0.0025€/stream'.format(round(total_streams, 2), round(total_streams*2.5, 2)))
+		stats.write('\n\nTotal streams of the day: {}k\nThat is {}€ with a 0.0025€ per stream ratio'.format(round(total_streams, 2), round(total_streams*2.5, 2)))
 	
 	file = File('daily_stats.txt')
 
-	embed.add_field(name='__**TOTAL**__', value='\n\nTotal des streams du jour : {}k\nSoit {}€ avec un ratio de 0.0025€/stream'.format(round(total_streams, 2), round(total_streams*2.5, 2)))
+	embed.add_field(name='__**TOTAL**__', value='\n\nTotal streams of the day: {}k\nThat is {} with a 0.0025€ per stream ratio'.format(round(total_streams, 2), round(total_streams*2.5, 2)))
 	embed.set_image(stats_ups)
-	hook_fin.send('**Stats du jour gros bg :money_with_wings:**', embed=embed, file=file)
+	hook_fin.send('**Daily statistics : :money_with_wings:**', embed=embed, file=file)
 
 
 
 connexion()
+
+
+############################################
+# CHECKER RUNNING 24/24 FOR NEW STATISTICS #
+############################################
+
 
 cycle = 0
 
@@ -124,25 +157,29 @@ while True:
 	try: 
 		driver.refresh()
 		time.sleep(5)
-		print('Nb de checks : {}'.format(cycle))
+		print('Amount of checks: {}'.format(cycle))
 		with open('last_date.txt', 'r') as date:
 			last_date = date.read()
+
 		wait = WebDriverWait(driver, 30)
 		wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'CircleShadow_shadow__G-Qkd')))
+
 		date_scrape_raw = driver.find_element_by_xpath('//*[@id="timeline"]/p')
 		date_scrape = date_scrape_raw.text[13:]
-		print('Dernière stats : {}\nStats du jour : {}'.format(last_date, date_scrape))
+		print('Last statistics: {}\nDaily statistics: {}'.format(last_date, date_scrape))
 		if date_scrape == last_date:
-			print('Les stats sont à jour')
+			print('Statistics are up to date')
 			time.sleep(600)
 		else:
-			print('Stats up ! Wait 5 min')
+			print('Statistics are up, wait 5 minutes !')
 			time.sleep(3 if cycle == 0 else 300) 
 			streams_artiste_logged()
-			streams_artists_liste()
-			print('GG à toi, tu as fait {}k streams le {}'.format(round(total_streams, 2), date_scrape))
+			streams_artists_list()
+
 			send()
-			print('Les stats ont été envoyées <3')
+			print('Statistics have been sent to your Discord')
+
+			# Writing date for the next cycle
 			with open('last_date.txt', 'w') as date:
 				date.write(date_scrape)
 			time.sleep(10)
@@ -152,5 +189,3 @@ while True:
 		reset()
 		pass
 	cycle += 1		
-	
-os.system('pause')
